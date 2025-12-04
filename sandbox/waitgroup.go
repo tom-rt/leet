f71234609ca3574ac0cc69065e80798ebd0d0d60
Thomas Raout <thomraout@gmail.com>
2025-12-04 21:06:21 +0100

    some dummy training

---
+++ sandbox/waitgroup.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func someCalc(wg *sync.WaitGroup, s int) {
+	time.Sleep(time.Second * time.Duration(s))
+	fmt.Printf("Done after %d seconds\n", s)
+	wg.Done()
+}
+
+func simpleWg() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go someCalc(&wg, 2)
+	wg.Add(1)
+	go someCalc(&wg, 4)
+	wg.Wait()
+}
